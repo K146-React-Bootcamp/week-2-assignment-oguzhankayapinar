f79811dc7e5d42d27a2ab68073f633a9f2420475
Oguzhan Kayapinar <oguzhankayapinar@gmail.com>
2022-09-30 13:47:44 +0300

    mission completed

--- js/todos.js
+++ js/todos.js
@@ -6,22 +6,33 @@ const editModal = document.querySelector("#editModal");
 let todos = [];
 let todo;
 
-const renderTodos = () => {
+// sayfa sayısı
+let current_page = 1;
+
+//her sayfada ne kadar satır bulunacağı
+let rows = 15;
+
+
+const renderTodos = (page = 1) => {
+	//page = 1 -> default olarak 1. sayfada olması için varsayılan değer.
 	root.innerHTML = "";
 	// todoları listele
 	const table = document.createElement("table");
 	table.setAttribute("class", "table table-hover");
-// yorum satırı denemesi 
-
-
 
 	const thead = document.createElement("thead");
+	
+	// title sıralaması yapmak için title-sorting id'si tanımlandı.
+	// id sıralaması yapmak için id-sorting id'si tanımlandı.
+	// kullanıcı id sıralaması yapmak için kulid-sorting id'si tanımlandı.
+	// Durum sıralaması yapmak için durum-sorting id'si tanımlandı
+	
 	thead.innerHTML = `
     <tr>
-      <th scope="col">id</th>
-      <th scope="col">Başlık</th>
-      <th scope="col">Kullanıcı Id</th>
-      <th scope="col">Durum</th>
+      <th scope="col" id="id-sorting">id<button> sırala</button> </th>
+      <th scope="col" id="title-sorting">Başlık <button>&darr;</button></th>
+      <th scope="col" id="kulid-sorting">Kullanıcı Id <button> sırala</button>  </th>
+      <th scope="col" id="durum-sorting">Durum<button> sırala</button></th>
       <th scope="col"></th>
     </tr>
   `;
@@ -46,7 +57,20 @@ const renderTodos = () => {
     `;
 		tbody.appendChild(tr);
 	};
-	todos.slice(0, 15).forEach((item) => {
+
+	// şu anki sayfanın 1 eksiği örneği 1. sayfaysa 0
+	page --;
+
+	// 10*0 = 0;
+	let start = rows * page;
+	console.log({start});
+	let end = start + rows;
+	// 0 + 10 = 10;
+	console.log({end});
+	// 0. index ve 10.index arası gösterilecek.
+	// hangi seçili sayfadaysak ona göre render ediliyor.
+	let paginatedItems = todos.slice(start, end);
+	paginatedItems.forEach((item) => {
 		renderItem(item);
 	});
 	table.appendChild(tbody);
@@ -57,7 +81,8 @@ const renderTodos = () => {
 			const id = Number(e.currentTarget.getAttribute("data-id"));
 			if (confirm("kaydı silmek istediğinize emin misiniz?")) {
 				todos = todos.filter((x) => x.id !== id);
-				renderTodos();
+				// aynı sayfada kalması için current_page parametresi eklendi.
+				renderTodos(current_page);
 			}
 		});
 	});
@@ -72,7 +97,64 @@ const renderTodos = () => {
 			editModal.classList.add("show");
 		});
 	});
+
+	document.querySelector('#title-sorting').addEventListener('click',() => {
+		// başlığa tıklandığında sıralama yapılacak.
+		todos.sort((a, b) => {
+			// küçük ve büyük harf farkını engellemek için
+			const nameA = a.title.toUpperCase(); // ignore upper and lowercase
+			const nameB = b.title.toUpperCase(); // ignore upper and lowercase
+			if (nameA < nameB) {
+			  return -1;
+			}
+			if (nameA > nameB) {
+			  return 1;
+			}
+		  
+			// names must be equal
+			return 0;
+		  });
+		  // sıralama yapılacak tekrar o sayfada render edilecek.
+		  renderTodos(current_page);
+	});
+
+
+	 // id'e göre sıralama yapmak için sort metodu 
+     document.querySelector('#id-sorting').addEventListener('click', () => {
+		todos.sort((a, b) =>  b.id - a.id)
+		renderTodos(current_page);
+	})
+
+	// kullanıcı id'e göre sıralama yapmak için sort metodu 
+	document.querySelector('#kulid-sorting').addEventListener('click', ()=>{
+		todos.sort((a, b)=> b.userId - a.userId)
+		renderTodos(current_page)
+	})
+	// duruma göre sıralama yapmak için sort metodu 
+	document.querySelector('#durum-sorting').addEventListener('click', ()=> {
+		todos.sort((a, b)=> { 
+			if(a.completed==true & b.completed==false){
+				return -1}	 
+		})
+		renderTodos(current_page)
+
+	})
+
+
 };
+
+
+document.querySelectorAll('.page-link').forEach((btn) => {
+	// pagination butonları içinde döndürülerek, her elemente click eklendi, tıklandığında kaçıncı sayfadaysa oraya göre render edilecek.
+	btn.addEventListener('click',() => {
+		let data_id = btn.getAttribute('data-id');
+		// kaçıncı buton olduğu attribute olarak alında.
+		current_page = Number(data_id);
+		renderTodos(current_page);
+	});
+});
+
+
 
 editModal.querySelector("#save").addEventListener("click", () => {
 	todo.title = editModal.querySelector("#title").value;
@@ -91,6 +173,7 @@ editModal.querySelectorAll(".close").forEach((button) => {
 	});
 });
 
+
 fetch(todosUrl)
 	.then((resp) => resp.json())
 	.then((data = []) => {
@@ -100,10 +183,3 @@ fetch(todosUrl)
 	.catch((error) => {
 		errorLogger(error);
 	});
-
-	// sıralama ödevi algoritması
-	// table thead kısmındaki sıralama yapılacak kolonlara event listener eklenecek.
-	// event listener hangi kolon için tıklanıyorsa
-	// sort metodu kullanılarak sıralama yapılacak
-	// sıralanmış todos'todus içerisine atılacak
-	// renderTodos metodu çalıştırılacak.
